9c83eebe79255469768cd1ebd5b5a50afea8be72
DaiDuong <daniel.do1992@gmail.com>
2019-06-29 16:13:05 +0700

    Add forms for CRUD

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -22,7 +22,11 @@ class BlogTests(TestCase):
 
     def test_string_representation(self):
         post = Post(title='Sample title')
+
         self.assertEqual(str(post), post.title)
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
 
     def test_post_content(self):
         self.assertEqual(str(self.post.title), 'Some title'),
@@ -31,6 +35,7 @@ class BlogTests(TestCase):
 
     def test_post_list_view(self):
         response = self.client.get(reverse('home'))
+
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Body content")
         self.assertTemplateUsed(response, 'home.html')
@@ -38,7 +43,32 @@ class BlogTests(TestCase):
     def test_post_detail_view(self):
         response = self.client.get('/post/1/')
         no_response = self.client.get('/post/9999999/')
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Some title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New test',
+            'author': self.user
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New test')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated body'
+        })
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(reverse('post_delete', args='1'))
+
+        self.assertEqual(response.status_code, 200)
